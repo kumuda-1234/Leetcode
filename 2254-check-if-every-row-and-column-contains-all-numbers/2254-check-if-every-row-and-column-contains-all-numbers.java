a7dd88f4af104396f95beb799753a7a509341acb
kumuda-1234 <kumuda1m@gmail.com>
2025-01-20 19:42:27 +0530

    Time: 6 ms (82.36%), Space: 45.2 MB (81.03%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
@@ -2,16 +2,20 @@ class Solution {
     public boolean checkValid(int[][] matrix) {
         int n = matrix.length;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             int[] rowCount = new int[n + 1];
             int[] colCount = new int[n + 1];
 
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) 
+            {
                 int rowVal = matrix[i][j];
                 int colVal = matrix[j][i];
 
-                if (rowVal < 1 || rowVal > n || rowCount[rowVal] > 0) return false;
-                if (colVal < 1 || colVal > n || colCount[colVal] > 0) return false;
+                if (rowVal < 1 || rowVal > n || rowCount[rowVal] > 0)
+                return false;
+                if (colVal < 1 || colVal > n || colCount[colVal] > 0) 
+                return false;
 
                 rowCount[rowVal]++;
                 colCount[colVal]++;
