4201db0d0e8a44e1e682f153cf8d3f850084096c
kumuda-1234 <kumuda1m@gmail.com>
2025-01-22 19:43:13 +0530

    Time: 32 ms (45.16%), Space: 51.4 MB (71.11%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,7 +3,8 @@ class Solution {
         List<List<Integer>>result = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
-        for(int i=0;i<n-2;i++){
+        for(int i=0;i<n-2;i++)
+        {
             if(i>0 && nums[i] == nums[i-1])
                 continue;
             int left = i+1;
