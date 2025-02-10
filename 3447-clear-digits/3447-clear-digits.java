9904365d2c2319e9808b1570b0a52eeea3adf1d0
kumuda-1234 <kumuda1m@gmail.com>
2025-02-10 20:02:42 +0530

    Time: 2 ms (74.08%), Space: 42.4 MB (93.3%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.java
+++ 3447-clear-digits/3447-clear-digits.java
@@ -1,12 +1,17 @@
 class Solution {
     public String clearDigits(String s) {
         StringBuilder res = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                if (res.length() > 0) {
-                    res.deleteCharAt(res.length() - 1); // Remove last char
+        for (char c : s.toCharArray()) 
+        {
+            if (Character.isDigit(c)) 
+            {
+                if (res.length() > 0) 
+                {
+                    res.deleteCharAt(res.length() - 1); 
                 }
-            } else {
+            } 
+            else 
+            {
                 res.append(c);
             }
         }
