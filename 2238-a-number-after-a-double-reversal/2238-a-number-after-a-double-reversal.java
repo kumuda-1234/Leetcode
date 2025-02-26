c9e8914456bd2dcb332d51b79f81a303f6e27abc
kumuda-1234 <kumuda1m@gmail.com>
2025-02-26 10:29:23 +0530

    Time: 0 ms (100%), Space: 40.4 MB (76.74%) - LeetHub

---
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        int count=0,rem;
+        if(num==0)
+        return true;
+        rem=num%10;
+        if(rem==0)
+        return false;
+        return true;
+        
+    }
+}
