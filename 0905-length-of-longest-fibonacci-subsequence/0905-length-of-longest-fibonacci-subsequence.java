96b4641bdff37260e2ea9df46a965513389dadac
kumuda-1234 <kumuda1m@gmail.com>
2025-02-27 22:05:14 +0530

    Time: 46 ms (97.34%), Space: 54.6 MB (53.85%) - LeetHub

---
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int lenLongestFibSubseq(int[] arr) {
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        int maxLen = 0;
+        
+        for (int i = 2; i< n; i++) 
+        {
+            int start = 0, end = i - 1;
+            while (start < end) 
+            {
+                int pairSum = arr[start] + arr[end];
+                if (pairSum > arr[i]) 
+                {
+                    end--;
+                } 
+                else if (pairSum < arr[i])
+                {
+                    start++;
+                }
+                 else 
+                 {
+                    dp[end][i] = dp[start][end] + 1;
+                    maxLen = Math.max(dp[end][i], maxLen);
+                    end--;
+                    start++;
+                }
+            }
+        }
+        return maxLen == 0 ? 0 : maxLen + 2;
+    }
+}
