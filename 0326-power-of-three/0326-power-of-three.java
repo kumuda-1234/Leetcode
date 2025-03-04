627bf18d02ae2b83670751e573c9db9e91729797
kumuda-1234 <kumuda1m@gmail.com>
2025-03-04 17:55:26 +0530

    Time: 8 ms (90.35%), Space: 44.3 MB (26.69%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        double p;
+        p=Math.log10(n)/Math.log10(3);
+        if(p-(int)p==0)
+        return true;
+        return false;
+        
+    }
+}
