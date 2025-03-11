91e7d23513f272474cf622d96cb75ed0877fbf7e
kumuda-1234 <kumuda1m@gmail.com>
2025-03-11 19:51:57 +0530

    Time: 11 ms (91.15%), Space: 44.7 MB (64.19%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -7,17 +7,23 @@ class Solution {
         while(right<s.length())
         {
             char ch=s.charAt(right);
-            if(ch=='a') a++;
-            else if(ch=='b') b++;
-            else if(ch=='c') c++;
+            if(ch=='a') 
+            a++;
+            else if(ch=='b')
+             b++;
+            else if(ch=='c')
+             c++;
             
             while(a>0 && b>0 && c>0 )
             {
                 count+=s.length()-right;
                 char ch1=s.charAt(left);
-                if(ch1=='a') a--;
-                else if(ch1=='b') b--;
-                else if(ch1=='c') c--;
+                if(ch1=='a') 
+                a--;
+                else if(ch1=='b') 
+                b--;
+                else if(ch1=='c')
+                 c--;
                 left++;
             }
             right++;
