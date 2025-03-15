07fa052ada0089fccac2d5c03685ced8c7fa3d66
kumuda-1234 <kumuda1m@gmail.com>
2025-03-15 11:06:42 +0530

    Time: 0 ms (100%), Space: 41.6 MB (87.05%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,13 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int i,ans=0;
+        for(i=0;i<32;i++)
+        {
+            int bit=n&1;
+            ans=(ans<<1)|bit;
+            n=n>>>1;
+        }
+        return ans;
+    }
+}
