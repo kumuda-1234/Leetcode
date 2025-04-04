f64ccb9e20617a375cfb17aa6ca527f15c8d39ef
kumuda-1234 <kumuda1m@gmail.com>
2025-04-04 13:43:50 +0530

    Time: 5 ms (72.39%), Space: 43.7 MB (97.81%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rem,rev=0,temp;
+        temp=x;
+        while(x>0)
+        {
+            rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+        if(temp==rev)
+        return true;
+        return false;
+        
+    }
+}
