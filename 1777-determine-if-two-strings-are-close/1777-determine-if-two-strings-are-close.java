0b9548b5a35004b05d53ac9419ec9a8dc1629e6b
kumuda-1234 <kumuda1m@gmail.com>
2025-05-11 22:19:27 +0530

    Time: 74 ms (21.12%), Space: 45.8 MB (52%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -1,29 +1,33 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-        for (char ch : word1.toCharArray()) 
-        {
-            freq1[ch - 'a']++;
+        int n = word1.length();
+        int m = word2.length();
+        HashMap<Character,Integer>mp1 = new HashMap<>();
+         HashMap<Character,Integer>mp2 = new HashMap<>();
+        if(m != n){
+            return false;
         }
-        for (char ch : word2.toCharArray()) 
+        for(int i=0;i<m;i++)
         {
-            freq2[ch - 'a']++;
+            char ch1 = word1.charAt(i);
+            char ch2 = word2.charAt(i);
+            mp1.put(ch1,mp1.getOrDefault(ch1,0)+1);
+            mp2.put(ch2,mp2.getOrDefault(ch2,0)+1);
+
         }
-        for (int i = 0; i < 26; i++) {
-            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) 
-                return false;
-            
-        }
-        Arrays.sort(freq1);
-        Arrays.sort(freq2);
-        for (int i = 0; i < 26; i++)
-        {
-            if (freq1[i] != freq2[i]) 
-            {
+        for(char key:mp1.keySet()){
+            if(!mp2.containsKey(key)){
                 return false;
             }
         }
-        return true;
+
+         List<Integer> freqList1 = new ArrayList<>(mp1.values());
+        List<Integer> freqList2 = new ArrayList<>(mp2.values());
+
+        Collections.sort(freqList1);
+        Collections.sort(freqList2);
+
+         return freqList1.equals(freqList2);
+
     }
 }
