be70892fdf8698f09c4e3724fe0cf74f692eb4a7
Kumuda Mahankali <kumuda1m@gmail.com>
2025-05-27 12:24:26 +0530

    Time: 30 ms (62.77%), Space: 55.4 MB (54.43%) - LeetHub

---
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -0,0 +1,24 @@
+class StockSpanner {
+    // Stack to store pairs of [price, span]
+    private Stack<int[]> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int cnt = 1; // Span starts at 1 (current day)
+
+        // Pop all prices from stack that are less than or equal to current price
+        while (!st.isEmpty() && price >= st.peek()[0]){
+            // Add their span to current span
+            cnt += st.pop()[1];
+        }
+
+        // Push the current price and its total span onto the stack
+        st.push(new int[]{price, cnt});
+
+        // Return the span for current price
+        return cnt;
+    }
+}
