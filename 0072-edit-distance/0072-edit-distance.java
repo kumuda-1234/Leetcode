a135d9582f393f74009970d6779201ae0ac848f3
Kumuda Mahankali <kumuda1m@gmail.com>
2025-06-01 17:43:59 +0530

    Time: 5 ms (62.56%), Space: 45.3 MB (30.98%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,40 +1,23 @@
 class Solution {
-    private int solve(int i, int j, String s1, String s2, int dp[][]) {
-        if (i == 0 && j == 0) {
-            if (s1.charAt(i) == s2.charAt(j))
-                return 0;
-            else
-                return 1;
-        }
-        if (j == -1)
-            return i + 1;
-        if (i == -1)
-            return j + 1;
+  public int minDistance(String word1, String word2) {
+    final int m = word1.length();//first word length
+    final int n = word2.length();///second word length
+    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
+    int[][] dp = new int[m + 1][n + 1];
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
+    for (int i = 1; i <= m; ++i)
+      dp[i][0] = i;
 
-        int insert = 300000000;
-        int replace = 30000000;
-        int delete = 30000000;
+    for (int j = 1; j <= n; ++j)
+      dp[0][j] = j;
 
-        if (s1.charAt(i) == s2.charAt(j)) 
-            return dp[i][j] = solve(i - 1, j - 1, s1, s2, dp);
-        else {
-            insert = 1 + solve(i, j - 1, s1, s2, dp);
-            replace = 1 + solve(i - 1, j - 1, s1, s2, dp);
-            delete = 1 + solve(i - 1, j, s1, s2, dp);
-        }
+    for (int i = 1; i <= m; ++i)
+      for (int j = 1; j <= n; ++j)
+        if (word1.charAt(i - 1) == word2.charAt(j - 1))//same characters
+          dp[i][j] = dp[i - 1][j - 1];//no operation
+        else
+          dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;                      //replace               //delete        //insert
 
-        return dp[i][j] = Math.min(insert, Math.min(replace, delete));
-    }
-
-    public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()][word2.length()];
-
-        for (int[] I : dp)
-            Arrays.fill(I, -1);
-
-        return solve(word1.length() - 1, word2.length() - 1, word1, word2, dp);
-    }
+    return dp[m][n];
+  }
 }
