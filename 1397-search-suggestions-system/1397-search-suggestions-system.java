2c577541a32932735096c0df674b16c6df61516e
Kumuda Mahankali <kumuda1m@gmail.com>
2025-06-03 14:51:49 +0530

    Time: 27 ms (50.16%), Space: 47 MB (80.36%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.java
+++ 1397-search-suggestions-system/1397-search-suggestions-system.java
@@ -1,64 +1,24 @@
 class Solution {
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        Trie trie = new Trie();
-        for(String word: products){
-            trie.insert(word);   
-        }
-
-        List<List<String>> suggestions = new ArrayList<>();
-        for(int i=0; i<searchWord.length(); i++){
-            suggestions.add(trie.match(searchWord.substring(0,i+1)));
-        }
-
-        return suggestions;
+public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+  PriorityQueue<String> pq = new PriorityQueue<>(3, (s1,s2) -> s1.compareTo(s2)); 
+  List<List<String>> list = new ArrayList<>();
+  
+  for(int i = 1; i<=searchWord.length(); i++){
+    String temp = searchWord.substring(0, i);
+    for(String s : products){
+      if(s.startsWith(temp)){
+        pq.offer(s);
+      }
     }
-
-    static class Node{
-        Node chars[] = new Node[26];
-        boolean isEnd = false;
-
-        public boolean contains(char ch){
-            return chars[ch - 'a'] != null;
-        }
+    List<String> temp_list = new ArrayList<>();
+    for(int j = 0; j<3; j++){
+      if(pq.peek() != null){
+        temp_list.add(pq.poll());
+      }
     }
-
-    static class Trie{
-        Node root;
-        Trie(){
-            root = new Node();
-        }
-
-        public void insert(String key){
-            Node temp = root;
-            for(char ch : key.toCharArray()){
-                if(!temp.contains(ch)) temp.chars[ch - 'a'] = new Node();
-                temp = temp.chars[ch - 'a'];
-            }
-            temp.isEnd = true;
-        }
-
-        public List<String> match(String key){
-            Node temp = root;
-            for(int i = 0; i < key.length(); i++){
-                char ch = key.charAt(i);
-                if(!temp.contains(ch)) return new ArrayList<>();
-                temp = temp.chars[ch - 'a']; 
-            }
-            StringBuilder sb = new StringBuilder(key);
-            return findAll(temp, sb, new ArrayList<>());
-        }
-
-        public List<String> findAll(Node temp, StringBuilder sb, List<String> list){
-            if(list.size() == 3) return list;
-            if(temp.isEnd) list.add(sb.toString());
-            for(int i = 0; i < 26; i++){
-                if(temp.chars[i] != null){
-                    sb.append((char)(i + 'a'));
-                    findAll(temp.chars[i], sb, list);
-                    sb.deleteCharAt(sb.length() - 1);
-                }
-            }
-            return list;
-        }
-    }
+    pq.clear();
+    list.add(temp_list);
+  }
+  return list;
+  }
 }
