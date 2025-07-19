6963470e7e6ccc9cd0a4c5c0eac02ccab0f7c8da
Kumuda Mahankali <kumuda1m@gmail.com>
2025-07-19 18:38:31 +0530

    Time: 0 ms (100%), Space: 40.6 MB (38.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,13 +5,12 @@ class Solution {
         int n1=1;
         int n2=2;
         int n3=0;
-        for(int i=2;i<n;i++)
+        for(int i=3;i<=n;i++)
         {
             n3=n1+n2;
             n1=n2;
             n2=n3;
         }
         return n3;
-        
     }
 }
