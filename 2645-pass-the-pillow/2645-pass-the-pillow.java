2d358505f9222692e757d5390207edbb80367772
Kumuda Mahankali <kumuda1m@gmail.com>
2025-07-25 06:39:11 +0530

    Time: 0 ms (100%), Space: 40.8 MB (12.39%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+         if (n == 1) 
+            return 1;
+        int cycleLength = 2 * (n - 1);
+         int effectiveTime = time % cycleLength;
+         if (effectiveTime < (n - 1)) 
+            
+            return 1 + effectiveTime;
+         else 
+         {
+            int stepsBackward = effectiveTime - (n - 1);
+            
+            return n - stepsBackward;
+
+
+         }
+            
+        
+    }
+}
