a42e2fe0a63dce335aeb3bbcd289bb3dabd38a8f
Kumuda Mahankali <kumuda1m@gmail.com>
2025-07-26 18:34:10 +0530

    Time: 0 ms (100%), Space: 40.6 MB (49.57%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,12 +1,12 @@
 class Solution {
     public int hammingDistance(int x, int y) {
         int count=0;
-        int res=x^y;
-        while(res!=0)
-        {
-            res=res&(res-1);
-            count++;
+        int n=x^y;
+        while (n != 0) {
+            count += (n & 1); // Check the least significant bit
+            n >>>= 1;         // Right shift by 1 (unsigned)
         }
         return count;
+        
     }
 }
