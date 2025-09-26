31cb49ed2c885aa35beaa2b4614a1f88fc6a2867
Kumuda Mahankali <kumuda1m@gmail.com>
2025-09-26 22:38:58 +0530

    Time: 7 ms (28.38%), Space: 46.2 MB (61.65%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,13 +1,17 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       int i,xr=0;
-       
-       for(i=0;i<nums.length;i++)
-       {
-        
-         xr=xr^nums[i];
-       } 
-       return xr;
-        
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i==nums.length-1){
+                return nums[i];
+            }
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+            else{
+                i++;
+            }
+        }
+        return 0;
     }
 }
