25aaf0c2ffe181643c38e1f3ebcc67348c175881
Kumuda Mahankali <kumuda1m@gmail.com>
2025-09-27 18:15:55 +0530

    Time: 0 ms (100%), Space: 41 MB (22.39%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -3,8 +3,8 @@ class Solution {
         int count=0;
         while(n!=0)
         {
-            n=n&(n-1);
-            count++;
+           count=count+(n&1);
+           n=n>>1;
         }
         return count++;
         
