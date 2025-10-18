8c9dcd99282a7fdb28a1cdb97daa82685c73e28f
Kumuda Mahankali <kumuda1m@gmail.com>
2025-10-18 22:50:28 +0530

    Time: 4 ms (76.42%), Space: 42.8 MB (94.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length())
         return false;
-        char ch[]=new char[26];
+        int ch[]=new int[26];
         for(int i=0;i<s.length();i++)
         {
             ch[s.charAt(i)-97]++;
