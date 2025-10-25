6947a1e08664eda3465404850a9ced974e27aafd
Kumuda Mahankali <kumuda1m@gmail.com>
2025-10-25 23:36:18 +0530

    Time: 2 ms (98.57%), Space: 42.3 MB (22.04%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,16 +1,26 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int num: arr)
-        {
-            if(map.containsKey(num))
-            map.put(num,map.get(num)+1);
-            else
-            map.put(num,1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num : arr) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                int freq = map.get(num);
+                freq++;
+                map.put(num, freq);
+            }
         }
-        Set<Integer> set =new HashSet<>(map.values());
-        if(map.size()==set.size())
+
+        for (Map.Entry<Integer, Integer> me : map.entrySet()) {
+            if (set.contains(me.getValue())) {
+                return false;
+            } else {
+                set.add(me.getValue());
+            }
+        }
+
         return true;
-        return false;
     }
 }
