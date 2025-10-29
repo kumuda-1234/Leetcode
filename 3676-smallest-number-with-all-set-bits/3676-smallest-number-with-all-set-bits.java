1dde417b966a1ee5d23923484b5bb4af44cac40a
Kumuda Mahankali <kumuda1m@gmail.com>
2025-10-29 18:14:46 +0530

    Time: 0 ms (100%), Space: 40.8 MB (46.73%) - LeetHub

---
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int smallestNumber(int n) {
+        int k=1;
+        int i;
+        while(k<n)
+        {
+            i=(1<<k);
+            if((i-1)>=n)
+            return i-1;
+            k++;
+        }
+        return ((1<<k)-1);
+    }
+}
