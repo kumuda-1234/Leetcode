14bf7593423f3ec6ed2620e6fe5e0c8faf676c42
Kumuda Mahankali <kumuda1m@gmail.com>
2025-12-25 12:01:00 +0530

    Time: 1 ms (70.14%), Space: 43.6 MB (8.59%) - LeetHub

---
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String winningPlayer(int x, int y) {
+        int ten=y/4;
+        int count=0;
+        while(x>0&&ten>0)
+        {
+            count++;
+            x--;
+            ten--;
+        }
+        if(count%2==0)
+        return "Bob";
+        return "Alice";
+   }
+}
