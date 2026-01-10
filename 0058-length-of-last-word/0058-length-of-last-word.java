544422a172f7ab79ccc1eaaa32cc87bf85de8494
Kumuda Mahankali <kumuda1m@gmail.com>
2026-01-10 20:39:02 +0530

    Time: 0 ms (100%), Space: 43.1 MB (55.91%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,13 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int n=s.length();
-        int i=n-1;
         int count=0;
-        while(i>=0 && s.charAt(i)==' ')
+        int i=n-1;
+        while(i>=0 && s.charAt(i)==' ' )
         i--;
         for(int j=i;j>=0 && s.charAt(j)!=' ';j--)
         {
             count++;
+
         }
         return count;
     }
