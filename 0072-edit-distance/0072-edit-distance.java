a7cc538d4fd95ecf3f3c73819229beb4f4520d65
Kumuda Mahankali <kumuda1m@gmail.com>
2026-01-17 14:41:59 +0530

    Time: 3 ms (96.67%), Space: 47.4 MB (10.71%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,34 +1,40 @@
 class Solution {
-    //memorization 
-    public int minDistance(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
-        int dp[][] = new int[m + 1][n + 1];
-        for(int row []:dp ){
-            Arrays.fill(row,-1);
+    private int solve(int i, int j, String s1, String s2, int dp[][]) {
+        if (i == 0 && j == 0) {
+            if (s1.charAt(i) == s2.charAt(j))
+                return 0;
+            else
+                return 1;
         }
+        if (j == -1)
+            return i + 1;
+        if (i == -1)
+            return j + 1;
 
-        return path(m , n ,s1, s2,dp);
-        
-    }
-    public int path(int i , int j,String s1,String s2,int dp[][]){
-
-        if(i == 0) return j;
-        if(j == 0) return i;
-
-        if(dp[i][j] != -1){
+        if (dp[i][j] != -1)
             return dp[i][j];
+
+        int insert = 300000000;
+        int replace = 30000000;
+        int delete = 30000000;
+
+        if (s1.charAt(i) == s2.charAt(j)) 
+            return dp[i][j] = solve(i - 1, j - 1, s1, s2, dp);
+        else {
+            insert = 1 + solve(i, j - 1, s1, s2, dp);
+            replace = 1 + solve(i - 1, j - 1, s1, s2, dp);
+            delete = 1 + solve(i - 1, j, s1, s2, dp);
         }
 
-        if(s1.charAt(i-1) == s2.charAt(j-1))
-        return dp[i][j] =  path(i-1, j-1 , s1, s2,dp);
-         
+        return dp[i][j] = Math.min(insert, Math.min(replace, delete));
+    }
 
-         // Characters don't match: choose min of insert, delete, or replace
-        int insert = path(i, j - 1, s1, s2, dp);
-        int delete = path(i - 1, j, s1, s2, dp);
-        int replace = path(i - 1, j - 1, s1, s2, dp);
+    public int minDistance(String word1, String word2) {
+        int dp[][] = new int[word1.length()][word2.length()];
 
-        return dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
+        for (int[] I : dp)
+            Arrays.fill(I, -1);
+
+        return solve(word1.length() - 1, word2.length() - 1, word1, word2, dp);
     }
 }
