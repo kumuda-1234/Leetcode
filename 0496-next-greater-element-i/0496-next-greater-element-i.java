c1f0535b368958d4e9aaa59d0c96a966941baee0
Kumuda Mahankali <kumuda1m@gmail.com>
2026-07-30 17:38:23 +0530

    Time: 4 ms (73.96%), Space: 45.2 MB (62.62%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,28 +1,26 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] result = new int[nums1.length]; 
-        for(int i=0; i<nums1.length; i++){
-            //pick a number 
-            int a = nums1[i];//[4,1,2]
-            //search 'a' in num2
-            int index = 0;
-            for(int j=0; j<nums2.length; j++)
-                if(a==nums2[j]) 
-                {
-                    index=j;
-                }
-              
-            int nge = -1; 
-            //now search for next greater element in nums2 
-            for(int k=index+1; k<nums2.length; k++){
-                if(nums2[k]>a)
-                {
-                    nge = nums2[k];
-                    break;
-                }
-            }
-            result[i]= nge;
-        }
-        return result;
+    int[] res = new int[nums1.length]; 
+    Stack<Integer> s1 = new Stack<>();
+    HashMap<Integer, Integer> hm = new HashMap<>();
+    //use stack and fill hashmap = nums2 
+    //[1,3,4,2]
+    for(int n:nums2){
+        while(!s1.isEmpty() && n>s1.peek()){
+            //fill the hashmap
+            hm.put(s1.pop(),n);
+        }    
+        s1.push(n);
+    }
+    //result is in hashmap = collect it and put in res 
+    //check in hashmap
+    //[4,1,2]  
+    int idx = 0;
+    for(int k:nums1){
+        if(hm.containsKey(k)) res[idx]=hm.get(k);
+        else res[idx] = -1;
+        idx++;
+    }
+    return res;
     }
 }
