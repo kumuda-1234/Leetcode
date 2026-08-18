6136a592593a7d18a2a65fe7434dfb3acd04219c
Kumuda Mahankali <kumuda1m@gmail.com>
2026-08-18 18:19:46 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,16 @@
 class Solution {
+    int[] tab=new int[46];
     public int climbStairs(int n) {
-        if(n==1||n==2)
-        return n;
-        int n1=1;
-        int n2=2;
-        int n3=0;
-        for(int i=3;i<=n;i++)
+        
+        tab[0]=1;
+        tab[1]=2;
+        
+        for(int i=2;i<n;i++)
         {
-            n3=n1+n2;
-            n1=n2;
-            n2=n3;
+            tab[i]=tab[i-1]+tab[i-2];
+           
         }
-        return n3;
+        return tab[n-1];
+        
     }
 }
